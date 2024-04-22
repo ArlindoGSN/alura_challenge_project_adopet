8753167909347f3813be1ea8eceb737232786714
ArlindoGSN <netonery06@gmail.com>
2024-04-21 21:20:03 -0300

    of alura_chalenge project adopet

---
+++ src/main/java/org/projeto/alura_challenges/AluraChallengesApplication.java
@@ -0,0 +1,13 @@
+package org.projeto.alura_challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraChallengesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/projeto/alura_challenges/AluraChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projeto.alura_challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
